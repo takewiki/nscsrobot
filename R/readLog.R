e828dcf673ba6ff1e58f8885cf533c6073357a76
takewiki <hulilei@takewiki.com.cn>
2019-07-21 17:45:57 +0800

    add the function

--- R/readLog.R
+++ R/readLog.R
@@ -22,4 +22,65 @@ read_kflog <- function(file='data-raw/input/2019-07-17.txt') {
 
 }
 
+#' 在原始log文件中删除会话符号----
+#'
+#' @param x 原始数据向量
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' log_delBash();
+log_delBash <- function(x){
+  res <-x[left(x,6) != '------'];
+  return(res);
+}
+
+#' 删除日志中的日期中的NA数据
+#'
+#' @param data  日期数据
+#'
+#' @return 删除dlg_datetime中的日期数据后行
+#' @export
+#'
+#' @examples
+#' log_delNA()
+log_delNA <- function(data) {
+  res <- data[!is.na(data$dlg_datetime), ];
+  return(res);
+}
+
+#' 删除数据中完全匹配keys的行记录
+#'
+#' @param data 数据
+#' @param keys  关键词
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' log_delRow_equals();
+log_delRow_equals <- function(data,keys) {
+  x <-data$logContent;
+  con <- !str_equals(x,keys);
+  res <- data[con,];
+  return(res);
+}
+#' 删除日志中包含多个关键词的行
+#'
+#' @param data 日志数据
+#' @param keys 多个关键词
+#'
+#' @return 返回数据
+#' @export
+#'
+#' @examples
+#' log_delRow_contains();
+log_delRow_contains <- function(data,keys) {
+  x <-data$logContent;
+  con <- !str_contains(x,keys);
+  res <- data[con,];
+  return(res);
+}
+
 
--- tests/test16.R
+++ tests/test16.R
@@ -3,38 +3,27 @@ library(tsdo);
 library(tsda);
 library(dplyr);
 
+
 data <-read_kflog(file = 'data-raw/input/2019-07-16.txt');
 log <- data$FkfLog;
 #' 针对数据进行删除处理，不包括------
-log <-log[left(log,6) != '------'];
-# 数据处理
-log[1:10];
+log <- log_delBash(log);
+# 针对日志进行分解，分为2列，日志内容及作者时间
+log_split <- str_splitByRighttBrace(x = log,suffix = ":");
+authorTime <- log_split[[1]];
+logContent <- log_split[[2]];
 
-
-log_split <-splitStr(log,"\\):");
-list_count <- length(log_split);
-authorTime <- character(list_count);
-logContent <- character(list_count);
-for (i in 1:list_count){
-  authorTime[i] <- log_split[[i]][1];
-  logContent[i] <- log_split[[i]][2];
-}
 #分离作者与datetime信息
-#authorTime;
-author_split <- splitStr(authorTime,"\\(");
-aut_count <- length(author_split);
-aut_id <-character(aut_count);
-dlg_datetime <- character(aut_count);
-for (j in 1:aut_count){
-  aut_id[j] <- author_split[[j]][1];
-  dlg_datetime[j] <- author_split[[j]][2];
-}
+aut_datetime <- str_splitByLeftBrace(authorTime);
+aut_id <- aut_datetime[[1]];
+dlg_datetime <- aut_datetime[[2]];
 
 res <- data.frame(aut_id,dlg_datetime,logContent,stringsAsFactors = F);
 #View(res);
 #处理异常数据，删除日期为NA为的记录
-res <- res[!is.na(res$dlg_datetime),];
+res <- res %>% log_delNA();
 #删除所有聊天记录为亲的记录
+keys_equal <-
 res <- res[res$logContent != '亲',];
 res <- res[res$logContent != "  亲",];
 #删除ID异常记录,
@@ -58,8 +47,23 @@ res$action_id[res$isA == FALSE] <- res$action_id[res$isA == FALSE] -1;
 # 数据进行分组处理
 g <-res$action_id;
 res <- split(res,g);
+# 针对多行数据进行合并；
+res <- lapply(res, log_combine_gp);
 
-class(res[[1]]);
-res[[2]];
+#将结合调整回一个数据框；
+res <- do.call("rbind",res);
+
+#将数据进一步进行处理
+#删除actionId
+res <- res[,c( "dlg_date","gp_id","session_id","isA","logContent")]
+res_q <- res[res$isA == FALSE,];
+res_q$question <- res_q$logContent;
+res_a <- res[res$isA == TRUE,];
+res_a$answer <- res_a$logContent;
+res <- df_innerJoin_bySameColNames(res_q,res_a,"session_id");
+res_name_sel <-c("dlg_date.x","gp_id.x","session_id","question","answer")
+res<-res[,res_name_sel];
+names(res) <-c("dlg_date","gp_id","session_id","question","answer")
+View(res);
 
 
